6cb5364b59d559dcdc06fb811e83085f037bfaf1
Fabi Tureo <fabianestebantureo@gmail.com>
2021-10-05 19:49:13 -0300

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import TestComponent from './TestComponent'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +17,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <TestComponent />
     </div>
   );
 }
